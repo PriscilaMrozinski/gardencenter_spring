c1465f73079e2be648678e86cdffffa6cb615218
Priscila Mrozinski <priscila_alins@hotmail.com>
2025-11-27 19:51:02 -0300

    Controller Categoria

--- src/main/java/com/comercial/gardencenter/controller/CategoriaController.java
+++ src/main/java/com/comercial/gardencenter/controller/CategoriaController.java
@@ -1,5 +1,79 @@
 package com.comercial.gardencenter.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.comercial.gardencenter.model.Categoria;
+import com.comercial.gardencenter.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+	// Injeção de dependencia:
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	// Listar todas as categorias
+	// Listar todas as categorias:
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll() {
+		return ResponseEntity.ok(categoriaRepository.findAll());
+	}
+
+	// Buscar pelo id:
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable Long id) {
+		return categoriaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	// Buscar pelo nome da categoria:
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Categoria>> getAllByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(categoriaRepository.findAllByNomecategoriaContainingIgnoreCase(nome));
+	}
+
+	// Criar categoria:
+	@PostMapping
+	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria) {
+		categoria.setIdcategoria(null);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+	}
+
+	// Atualizar:
+	@PutMapping
+	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria) {
+		return categoriaRepository.findById(categoria.getIdcategoria())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(categoriaRepository.save(categoria)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	// Deletar:
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		if (categoria.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		categoriaRepository.deleteById(id);
+	}
 
 }
